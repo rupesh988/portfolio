e37d2cef07fcbf0018eb2876a03479270ea29488
M Rupesh Kumar Reddy <inrupesh.in@gmail.com>
2025-05-24 14:40:23 +0530

    deploy

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import React, { useEffect } from 'react'
+import './index.css';
 
-export default function App() {
-  useEffect(()=>{
-    const ch = () => console.log(window.scrollY)
-    window.addEventListener('scrollend',ch)
-    return () => window.removeEventListener('volumechange',ch) 
-  },[])
+function App() {
   return (
-    <div className='bg-yellow-50 h-screen w-screen overflow-x-scroll scroll-smooth snap-y snap-mandatory '>
-        <section className='bg-green-50 h-screen w-screen snap-start justify-center ' >
-          <p>hello</p>
-        </section>
-        <section className='bg-green-100 h-screen w-screen snap-start justify-center ' >
-          <p>hello</p>
-        </section>
-        <section className='bg-green-200 h-screen w-screen snap-start justify-center ' >
-          <p>hello</p>
-        </section>
-        
+    <div className="bg-amber-100 flexflex-col " >
+      <div className='flex justify-center items-center flex-col h-screen ' onScroll={()=>console.log("scrolled ")} >
+      <div className='flex-row flex absolute top-6  gap-2 font-medium '>
+        <a href='#k'>About</a>
+        <a href='#k'>Work</a>
+        <a href='#k'>Resume</a>
+      </div>
+      <div className='bg-black absolute top-4 right-4 rounded-3xl hover:bg-gray-800 shadow shadow-red-400  '>
+        <p className='text-amber-100 m-2 font-medium mr-4 ml-4 '>CONTACT ME</p>
+      </div>
+      <h1 onMouseOver={()=>console.log("hello")} className='text-7xl font-bold select-none' >R U P E S H</h1>
+      <p className='text-black mt-3  font-thin hover:underline select-none' > Developer | Innovator | Dreamer </p>
+      <p className='  absolute bottom-2 '>↑</p>
+      </div>
+
+      <div className='h-screen bg-black' id='k' >
+
+      </div>
+
     </div>
-  )
+
+  );
 }
+
+export default App;
