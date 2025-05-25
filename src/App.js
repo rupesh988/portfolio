3f9926a4f1ed6b09c12bc21783ae6386f9c0e32f
M Rupesh Kumar Reddy <inrupesh.in@gmail.com>
2025-05-25 07:59:25 +0530

    changed contact,Name compoenet

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="bg-green-100 flex flex-col scroll-smooth">
 
-      {/* NAVIGATION BAR */}
+
       <div className='flex justify-center items-center flex-col h-screen'>
         <div className='flex-row flex absolute top-6 gap-4 font-medium'>
           <a href='#about' className='hover:underline'>About</a>
@@ -16,18 +16,18 @@ function App() {
           <a href='https://github.com/rupesh988/portfolio/blob/master/Rupeshres.pdf'  className='hover:underline'>Resume</a>
         </div>
 
-        {/* CONTACT BUTTON */}
-        <a href="#con"><div className='bg-black absolute top-4 right-4 rounded-3xl hover:bg-gray-800 shadow shadow-red-400'>
-          <p className='text-amber-50 m-2 font-medium mr-4 ml-4'>CONTACT ME</p>
-        </div></a>
+        <a href="#con">
+          <div className='bg-black fixed bottom-4 right-4 md:top-4 md:right-4 md:bottom-auto rounded-3xl hover:bg-gray-800 shadow shadow-red-400'>
+            <p className='text-amber-50 m-2 font-medium mr-4 ml-4'>CONTACT ME</p>
+          </div>
+        </a>
 
-        {/* TITLE SECTION */}
-        <h1 onMouseOver={() => console.log("hello")} className='text-7xl font-bold select-none'>R U P E S H</h1>
+
+        <h1 onMouseOver={() => console.log("hello")} className='text-4xl sm:text-5xl md:text-7xl font-bold select-none'>R U P E S H</h1>
         <p className='text-black mt-3 font-thin hover:underline select-none'>Developer | Innovator | Dreamer</p>
         <p className='absolute bottom-2'>↑</p>
       </div>
 
-      {/* SECTION COMPONENTS WITH IDS */}
       <div id="about">
         <About />
       </div>
